a1e9f0343b1c737d47cccc0dc3a00f5e40f0e857
Sarthak Singh <sarthaksingh2010@gmail.com>
2019-02-27 21:26:53 +0530

    Create TypesAndVariables.java

---
+++ basics/TypesAndVariables.java
@@ -0,0 +1,22 @@
+public class TypesAndVariables {
+	//main hit ctrl+space
+	public static void main(String[] args) {
+		//byte short int long float double char boolean
+		int myvar=10;
+		double myvar2=23.55;
+		float myvar6=45.4f;
+		String myvar3="Sarthak";
+		char myvar4='A';
+		boolean myvar5=true;
+		int a=Integer.MAX_VALUE;
+		int b=Integer.MIN_VALUE;
+		//Sysout
+		System.out.println(myvar);
+		System.out.println(myvar2);
+		System.out.println(myvar3);
+		System.out.println(myvar4);
+		System.out.println(myvar5);
+		System.out.println(myvar6);
+		System.out.println("Range of int: "+a+" "+b);
+	}
+}
