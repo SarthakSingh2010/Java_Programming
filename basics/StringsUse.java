6db8197e7abdb9a02ad69b39310f28e61fd70130
Sarthak Singh <sarthaksingh2010@gmail.com>
2019-02-27 21:26:49 +0530

    Create StringsUse.java

---
+++ basics/StringsUse.java
@@ -0,0 +1,17 @@
+public class StringsUse { 
+	public static void main(String[] args) {  
+		String t="Hello"; //Create a Sting 
+		int a=10; //Integer  variable
+		String blank=" "; // blank String
+		String name="Sarthak";
+		
+		String greet=t+blank+name; //String Concatenation
+		System.out.println(greet); //Print String
+		
+		System.out.println("Hello"+" "+"this is Java.");
+		System.out.println("My Integer :: "+a);// Print String along with Integer
+		
+		double b=1234.566789;
+		System.out.println("My Double :: "+b);	// Print String with Double
+	} 
+} 
