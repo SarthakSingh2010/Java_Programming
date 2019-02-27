13c1ac4b510617ff32e983d053591fb825d40d0d
Sarthak Singh <sarthaksingh2010@gmail.com>
2019-02-27 21:26:30 +0530

    Create HelloWorld.java

---
+++ basics/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld { //class	
+	public static void main(String[] args)
+	{ //method
+		System.out.println("Hello World!");
+		//Format Specifier for String
+		System.out.printf("%s\n","Hello World!");
+		//Format Specifier for Integer base 10 type
+		System.out.printf("%d\n",10);
+		//Format Specifier for Floating Point Numbers
+		System.out.printf("%.2f\n",123.456);
+		//Format Specifier for Character
+		System.out.printf("%c\n",'a');	
+	}// method close
+}// class closed
