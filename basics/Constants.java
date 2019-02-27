75511e241f4f4de2b37684c19e8cbfabfc1ee170
Sarthak Singh <sarthaksingh2010@gmail.com>
2019-02-27 21:26:25 +0530

    Create Constants.java

---
+++ basics/Constants.java
@@ -0,0 +1,9 @@
+public class Constants {
+	public static final int WIDTH=800;
+	public static final int HEIGHT=600;
+	public static void main(String[] args) {
+		System.out.println(Math.PI);//static member
+		System.out.println(WIDTH);
+		
+	}
+}
