ad53d839c38135835fb6c11746357bf716067934
Sarthak Singh <sarthaksingh2010@gmail.com>
2019-03-02 12:12:04 +0530

    Create CompetitionPlatform02 .java

---
+++ Intermediate/CompetitionPlatform02 .java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class CompetitionPlatform02 
+{
+	static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	static Scanner sc=new Scanner(System.in);
+	static PrintWriter pw= new PrintWriter(System.out);
+	public static void main(String[] args)throws IOException	
+	{
+		pw.println("Hello Compatition");
+		pw.close();
+	}
+	
+}
