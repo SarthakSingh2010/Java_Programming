567df03ebdd4b7cb136c85255125e2afe5d0c8a1
Sarthak Singh <sarthaksingh2010@gmail.com>
2019-04-09 19:22:17 +0530

    Create series9.java

---
+++ exam/series9.java
@@ -0,0 +1,13 @@
+class series9
+{
+public static void main(String args[])
+{
+int i,j,k;
+for( i=5;i>=1;i--)
+{
+for( j=5;j>=i;j--)
+System.out.print(j);
+for(k=1;k<=i-1;k++)
+System.out.print(i);
+System.out.println();
+}}}
