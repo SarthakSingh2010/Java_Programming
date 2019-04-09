ecc1483111cea24400ac02ffb7499fdda3f5685d
Sarthak Singh <sarthaksingh2010@gmail.com>
2019-04-09 19:22:14 +0530

    Create series3.java

---
+++ exam/series3.java
@@ -0,0 +1,17 @@
+class series3
+    {
+        public static void main(String args[])
+        {
+            int i,j;
+            int  a=1;
+            for(i=6;i>=1;i--)
+            {
+                for(j=6;j>=a;j--)
+                {
+                    System.out.print(j);
+                }
+                    System.out.println( );
+                    a=a+1;
+            }
+        }
+    }   
