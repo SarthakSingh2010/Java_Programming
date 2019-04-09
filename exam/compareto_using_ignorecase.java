731dd8d1de6bbd617282e24a3efb5d14c80a478f
Sarthak Singh <sarthaksingh2010@gmail.com>
2019-04-09 19:20:46 +0530

    Create compareto_using_ignorecase.java

---
+++ exam/compareto_using_ignorecase.java
@@ -0,0 +1,11 @@
+class compareto_using_ignorecase
+
+{
+    public static void main(String s1,String s2)
+    {
+          int b=s1.compareToIgnoreCase(s2);
+          System.out.println(b);
+        
+    }
+}
+    
