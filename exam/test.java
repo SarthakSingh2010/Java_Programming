ed597fba11743925fc3cb2bd611eb6b1bab35180
Sarthak Singh <sarthaksingh2010@gmail.com>
2019-04-09 19:22:27 +0530

    Update test.java

--- exam/test.java
+++ exam/test.java
@@ -1,47 +1,12 @@
-import java.util.*;
-public class test
+class test
 {
+    static int x=3;
+    int y=4;
     public static void main(String args[])
     {
         test obj=new test();
-        //System.out.println("input");
-        Scanner sc=new Scanner(System.in);
-        int simon=sc.nextInt();
-        int antisimon=sc.nextInt();
-        int totalheap=sc.nextInt();
-        int heaptake=0;
-        int heapleft=0;
-        int flag=0;
-        while(heaptake<=heapleft)
-        {
-            if(flag==0)
-            {
-                heaptake=obj.gcd(simon,totalheap);
-                heapleft=totalheap-heaptake;
-                totalheap=heapleft;
-                flag=1;
-            }
-            else
-            {
-                heaptake=obj.gcd(antisimon,totalheap);
-                heapleft=totalheap-heaptake;
-                totalheap=heapleft;
-                flag=0;
-            }
-        }
-        if(flag==1)
-        System.out.println("0");
-        else
-        System.out.println("1");
-    }
-    public  int gcd(int m,int n)
-    {
-        int r=0;
-        for(int i=1;i<=Math.min(m,n);i++)
-        {
-            if(m%i==0&&n%i==0)
-            r=i;
-        }
-        return r;
+        System.out.println("x value "+test.x);
+        System.out.println("y value "+obj.y);
     }
 }
+        
