7921d3d0d1500b46831cc26f1bd81128beda42cb
Sarthak Singh <sarthaksingh2010@gmail.com>
2019-04-09 19:21:40 +0530

    Create pattern1.java

---
+++ exam/pattern1.java
@@ -0,0 +1,21 @@
+class pattern1
+{
+    public static void main(String args[])
+    {
+        int i,k;
+        int j,x=5,y=4,n=-1,m=4;
+        for(i=5;i>=1;i--)
+        {
+                for(j=0;j<i;j++)
+                {
+                    System.out.print(x);
+                }
+                     for(k=0;k<=n;k++)
+                     {
+                         System.out.print(y);
+                      }
+           System.out.println( );
+           n++;
+       }
+    }
+}
